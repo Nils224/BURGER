c9fd0779dc3a1e824e0b902b0192be13327231e0
Nilesh Patel <npatel224@gmail.com>
2020-02-29 10:28:02 +0000

    updated credentials

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
   port: 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS,
